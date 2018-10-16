571a4fda1c14cb5642aa274c23634e9d3870f62e
Andrew Mc Cormack <andrewm@cyber-duck.co.uk>
2018-10-16 17:17:32 +0100

    add Deleteable trait

---
+++ src/Traits/Deleteable.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace CyberDuck\Pardot\Traits;
+
+/**
+ * Trait to allow the deletion of specific object types in a generic way
+ * 
+ * @category   PardotApi
+ * @package    PardotApi
+ * @author     Andrew Mc Cormack <andy@cyber-duck.co.uk>
+ * @copyright  Copyright (c) 2018, Andrew Mc Cormack
+ * @license    https://github.com/Cyber-Duck/Pardot-API/license
+ * @version    1.0.0
+ * @link       https://github.com/Cyber-Duck/Pardot-API
+ * @since      1.0.0
+ */
+trait Deleteable
+{
+    /**
+     * Sends the request to delete the object
+     * 
+     * /api/{object}/version/{version}/do/delete/id/<id>?...
+     * 
+     * required: user_key, api_key, id
+     *
+     * @param int $id
+     * @return int
+     * @todo returns HTTP 204 No Content on success
+     */
+    public function delete(int $id):? stdClass
+    {
+        return $this->setOperator(sprintf('delete/id/%s', $id))->request($this->object);
+    }
+}
