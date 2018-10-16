7dc0f9c3508d0aa5ab13ace3e3a9d3ea31da85b5
Andrew Mc Cormack <andrewm@cyber-duck.co.uk>
2018-10-16 16:51:50 +0100

    add QueryObject interface

---
+++ src/Contract/QueryObject.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace CyberDuck\Pardot\Contract;
+
+interface QueryObject
+{
+    /**
+     * Sends the request to query the object results and returns them from the API
+     * 
+     * /api/{operator}/version/{version}/do/query?...
+     * 
+     * required: user_key, api_key
+     * 
+     * @param array $criteria
+     * @return array|null
+     */
+    public function query(array $criteria):? array;
+
+    /**
+     * Returns an array of allowed query criteria options
+     *
+     * @return array
+     */
+    public function getQueryCriteria(): array;
+
+    /**
+     * Returns an array of allowed query navigation options
+     *
+     * @return array
+     */
+    public function getQueryNavigation(): array;
+}
