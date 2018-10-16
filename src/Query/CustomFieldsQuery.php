b0c6a418445a954349b6eda8d28f96b568c15742
Andrew Mc Cormack <andrewm@cyber-duck.co.uk>
2018-10-16 17:18:29 +0100

    add CustomFieldsQuery class

---
+++ src/Query/CustomFieldsQuery.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace CyberDuck\Pardot\Query;
+
+use CyberDuck\Pardot\Contract\QueryObject;
+use CyberDuck\Pardot\Traits\Createable;
+use CyberDuck\Pardot\Traits\Deleteable;
+use CyberDuck\Pardot\Traits\Queryable;
+use CyberDuck\Pardot\Traits\Readable;
+use CyberDuck\Pardot\Traits\Updateable;
+use CyberDuck\Pardot\Validator\DateValidator;
+use CyberDuck\Pardot\Validator\PositiveIntValidator;
+use CyberDuck\Pardot\Validator\SortOrderValidator;
+
+/**
+ * Custom Fields object representation
+ * 
+ * @category   PardotApi
+ * @package    PardotApi
+ * @author     Andrew Mc Cormack <andy@cyber-duck.co.uk>
+ * @copyright  Copyright (c) 2018, Andrew Mc Cormack
+ * @license    https://github.com/Cyber-Duck/Pardot-API/license
+ * @version    1.0.0
+ * @link       https://github.com/Cyber-Duck/Pardot-API
+ * @since      1.0.0
+ */
+class CustomFieldsQuery extends Query implements QueryObject
+{
+    use Queryable, Readable, Updateable, Createable, Deleteable;
+
+    /**
+     * Object name
+     *
+     * @var string
+     */
+    protected $object = 'customField';
+
+    /**
+     * Returns an array of allowed query criteria and validators for the values
+     *
+     * @return array
+     */
+    public function getQueryCriteria(): array
+    {
+        return [
+            'created_after'   => new DateValidator,
+            'created_before'  => new DateValidator,
+            'id_greater_than' => new PositiveIntValidator,
+            'id_less_than'    => new PositiveIntValidator
+        ];
+    } 
+
+    /**
+     * Returns an array of allowed query navigation params and validators for the values
+     *
+     * @return array
+     */
+    public function getQueryNavigation(): array
+    {
+        return [
+            'limit'      => new PositiveIntValidator,
+            'offset'     => new PositiveIntValidator,
+            'sort_by'    => new FixedValuesValidator('created_at', 'id', 'name'),
+            'sort_order' => new SortOrderValidator
+        ];
+    }
+}
