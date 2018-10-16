6ec32ed44272a469320ad56b8f63806fc60fc793
Andrew Mc Cormack <andrewm@cyber-duck.co.uk>
2018-10-16 19:01:17 +0100

    add EmailTemplatesQuery class

---
+++ src/Query/EmailTemplatesQuery.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace CyberDuck\Pardot\Query;
+
+use CyberDuck\Pardot\Traits\CanRead;
+
+/**
+ * Email Templates object representation
+ * 
+ * @category   PardotApi
+ * @package    PardotApi
+ * @author     Andrew Mc Cormack <andy@cyber-duck.co.uk>
+ * @copyright  Copyright (c) 2018, Andrew Mc Cormack
+ * @license    https://github.com/Cyber-Duck/Pardot-API/license
+ * @version    1.0.0
+ * @link       https://github.com/Cyber-Duck/Pardot-API
+ * @since      1.0.0
+ */
+class EmailTemplatesQuery extends Query
+{
+    use CanRead;
+
+    /**
+     * Object name
+     *
+     * @var string
+     */
+    protected $object = 'emailTemplate';
+
+    /**
+     * Returns a list of email templates which are enabled for use in one to one emails.
+     * 
+     * /api/emailTemplate/version/{version}/do/listOneToOne
+     * 
+     * required: user_key, api_key
+     *
+     * @return array|null
+     */
+    public function listOneToOne():? array
+    {
+        return $this->setOperator('listOneToOne')->request($this->object);
+    }
+}
