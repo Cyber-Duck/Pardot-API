f1e64ba9759ddb7f5e8409dc8f4d9f7831e4635a
Andrew Mc Cormack <andrewm@cyber-duck.co.uk>
2018-10-16 23:12:06 +0100

    add BooleanValidator class

---
+++ src/Validator/BooleanValidator.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace CyberDuck\Pardot\Validator;
+
+/**
+ * Boolean validation class
+ * 
+ * @category   PardotApi
+ * @package    PardotApi
+ * @author     Andrew Mc Cormack <andy@cyber-duck.co.uk>
+ * @copyright  Copyright (c) 2018, Andrew Mc Cormack
+ * @license    https://github.com/Cyber-Duck/Pardot-API/license
+ * @version    1.0.0
+ * @link       https://github.com/Cyber-Duck/Pardot-API
+ * @since      1.0.0
+ */
+class BooleanValidator extends Validator
+{
+    /**
+     * Validation method
+     *
+     * @param mixed $value
+     * @return boolean
+     */
+    public function validate($value): bool
+    {
+        return is_bool($value);
+    }
+}
