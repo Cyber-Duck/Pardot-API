6e67ed0201a80cb490b020cf57e96bb439580247
Andrew Mc Cormack <andrewm@cyber-duck.co.uk>
2018-10-17 01:48:56 +0100

    add bare classes

---
+++ src/Query/ListsQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace CyberDuck\Pardot\Query;
+
+/**
+ * List Query object representation
+ * 
+ * @category   PardotApi
+ * @package    PardotApi
+ * @author     Andrew Mc Cormack <andy@cyber-duck.co.uk>
+ * @copyright  Copyright (c) 2018, Andrew Mc Cormack
+ * @license    https://github.com/Cyber-Duck/Pardot-API/license
+ * @version    1.0.0
+ * @link       https://github.com/Cyber-Duck/Pardot-API
+ * @since      1.0.0
+ */
+class ListsQuery extends Query
+{
+
+}
